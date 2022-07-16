dc8dd9bd64b91358530d7f3b71cde2f96e7dd4ec
warlock <h.kwiatkowski@hotmail.com>
2022-07-16 21:16:07 +0200

    finished concat()

---
+++ ex11/src/index.js
@@ -0,0 +1,14 @@
+const string1 = "Say Cheese, "
+const string2 = "Konrad"
+
+console.log(string1.concat(string2))
+
+const array1 = [1,2,3]
+const array2 = [4,5,6]
+const array3 = [7,8,9]
+const newArray = array1.concat(array2, array3)
+
+console.log(array1)
+console.log(array2)
+console.log(array3)
+console.log(newArray)
